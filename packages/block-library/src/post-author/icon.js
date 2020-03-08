9cfc14e52b0d886334939e1515f41e38b9d9338d
Andrei Draganescu <andrei.draganescu@automattic.com>
2020-03-08 16:49:50 +0200

    adds icon

---
+++ packages/block-library/src/post-author/icon.js
@@ -0,0 +1,15 @@
+/**
+ * WordPress dependencies
+ */
+import { Circle, Path, SVG } from '@wordpress/components';
+
+export default (
+	<SVG width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<Path
+			d="M13 12v-1a2 2 0 00-2-2H7a2 2 0 00-2 2v1M4 19.25h9M4 15.25h16"
+			stroke="#000"
+			stroke-width="1.5"
+		/>
+		<Circle cx="9" cy="5" r="2" fill="#000" />
+	</SVG>
+);
