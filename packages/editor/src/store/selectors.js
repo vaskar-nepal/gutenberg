718573cedf69a830ac650833957e8eb13fcd8933
William Earnhardt <wearnhardt@gmail.com>
2020-03-15 20:49:29 -0400

    Include null date check on isEditedPostDateFloating()

--- packages/editor/src/store/selectors.js
+++ packages/editor/src/store/selectors.js
@@ -759,7 +759,7 @@ export function isEditedPostDateFloating( state ) {
 		status === 'auto-draft' ||
 		status === 'pending'
 	) {
-		return date === modified;
+		return date === modified || date === null;
 	}
 	return false;
 }
